bd3f4175baa5be844985f38f77da0632f7382809
nassipkaliv <nassipkaliv@gmail.com>
2023-03-26 00:41:52 +0500

    foodappui-rn

---
+++ assets/colors/colors.js
@@ -0,0 +1,12 @@
+const colors = {
+  backgroudn: '#f9f9fb',
+  textDark: '#313234',
+  primary: '#f5ca48',
+  secondary: '#f26c68',
+  textLight: '#cdcdcd',
+  price: '#e4723c',
+  white: '#fff',
+  black: '#000',
+};
+
+export default colors;
